e8e5b24dc30b510567355eb2626770cae49e1613
rochamrcs <mavinicius95@gmail.com>
2025-03-02 21:56:21 -0300

    Implementação de efeitos de transição na barra de navegação

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// trocar as cores da sessão atual na barra de navegação
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height)  {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            })
+        }
+    })
+
+    // adicionar borda na barra de navegação
+    let header = document.querySelector('header')
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
